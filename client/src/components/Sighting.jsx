42e0aee04f0598086d38222d6dca13d09e6719c2
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-20 14:02:07 -0500

    Shows date in human-readable format

--- client/src/components/Sighting.jsx
+++ client/src/components/Sighting.jsx
@@ -9,7 +9,7 @@ const Sighting = ({sighting}) => {
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{sighting.species} {sighting.location} {sighting.datetime} {sighting.healthy} {sighting.sighter_email} </Card.Title>
+            <Card.Title>{sighting.species} {sighting.location} {moment(sighting.datetime).format("MMMM Do, YYYY")} {sighting.healthy} {sighting.sighter_email} </Card.Title>
             {/* <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button> */}
             {/* <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button> */}
             </Card.Body>
