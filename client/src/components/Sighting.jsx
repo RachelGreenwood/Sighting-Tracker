f1cd9eb0874c8eb5530a8ff8ce8478a4d7252c80
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-20 14:00:17 -0500

    Shows sighting table data on front-end

--- client/src/components/Sighting.jsx
+++ client/src/components/Sighting.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import * as ioicons from 'react-icons/io5'
+import moment from 'moment';
 
 const Sighting = ({sighting}) => {
 
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{sighting.species} {sighting.location}</Card.Title>
+            <Card.Title>{sighting.species} {sighting.location} {sighting.datetime} {sighting.healthy} {sighting.sighter_email} </Card.Title>
             {/* <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button> */}
             {/* <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button> */}
             </Card.Body>
