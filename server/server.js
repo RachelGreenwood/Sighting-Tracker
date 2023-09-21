ba5e90efe717946ff6c524b3064add0af1b85a77
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-20 21:51:54 -0500

    Shows results from joined table

--- server/server.js
+++ server/server.js
@@ -18,7 +18,21 @@ app.get('/', (req, res) => {
 // create the get request for students in the endpoint '/api/students'
 app.get('/sightings', async (req, res) => {
     try {
-        const { rows: animals } = await db.query('SELECT * FROM sightings');
+        const { rows: animals } = await db.query(`SELECT
+        s.id,
+        s.datetime,
+        s.species,
+        s.location,
+        s.healthy,
+        s.sighter_email,
+        s.timestamp AS sighting_timestamp,
+        a.nickname
+    FROM
+        sightings s
+    LEFT JOIN
+        animals a
+    ON
+        s.species = a.species`);
         res.send(animals);
     } catch (e) {
         return res.status(400).json({ e });
@@ -47,42 +61,6 @@ app.get('/sightings', async (req, res) => {
 //     }
 
 // });
-
-// // delete request for students
-// app.delete('/api/students/:studentId', async (req, res) => {
-//     try {
-//         const studentId = req.params.studentId;
-//         await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-//         console.log("From the delete request-url", studentId);
-//         res.status(200).end();
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(400).json({ e });
-
-//     }
-// });
-
-// //A put request - Update a student 
-// app.put('/api/students/:studentId', async (req, res) =>{
-//     //console.log(req.params);
-//     //This will be the id that I want to find in the DB - the student to be updated
-//     const studentId = req.params.studentId
-//     const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-//     console.log("In the server from the url - the student id", studentId);
-//     console.log("In the server, from the react - the student to be edited", updatedStudent);
-//     // UPDATE students SET lastname = "something" WHERE id="16";
-//     const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-//     const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-//     try {
-//       const updated = await db.query(query, values);
-//       console.log(updated.rows[0]);
-//       res.send(updated.rows[0]);
-  
-//     }catch(e){
-//       console.log(e);
-//       return res.status(400).json({e})
-//     }
-//   })
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
